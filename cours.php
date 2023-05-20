8b2fd1e3629c6795e23766ae6e07013bdeb03993
COLLIDORLionel <lionel.collidor2016@campus-eni.fr>
2023-05-20 19:40:48 +0200

    ajout image pour future page 404, séparation navbar pour éviter la répétition, mise à jour des pages pour inclure la navbar et passage des pages de .html à .php

--- cours.php
+++ cours.php
@@ -11,29 +11,7 @@
 
 <body>
 
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="index.html">School'prog</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link" href="cours.html">Cours</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="creer-cours.html">Créer un cours</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Connexion</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-    </header>
+    include_once('inc/menu.tpl.php');
 
     <main class="container mb-5">
 
